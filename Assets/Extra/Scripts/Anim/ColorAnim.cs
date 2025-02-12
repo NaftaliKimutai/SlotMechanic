32a6de61c12730e3c4142ad895ea3e9dd93b3ddf
NaftaliKimutai <75565738+NaftaliKimutai@users.noreply.github.com>
2025-02-12 10:27:23 +0300

    update

--- Assets/Extra/Scripts/Anim/ColorAnim.cs
+++ Assets/Extra/Scripts/Anim/ColorAnim.cs
@@ -6,45 +6,73 @@ public class Color_animdata
 {
     public float TargetTime = 0.1f;
     public float Speed = 2f;
-    public Color TargetColor=Color.white;
+    public Color TargetColor = Color.white;
 }
 public class ColorAnim : MonoBehaviour
 {
+    public bool IsRandom;
+    public bool _IsLoop = true;
     float timestamp;
     public int Target;
+    public float StartOffsetTime;
     public Color_animdata[] TargetData;
     public Image TheImg;
     public TMP_Text TheText;
+    public Outline TheOutline;
     Color TheColor;
     float Speed;
     private void OnEnable()
     {
+        ResetAnim();
+    }
+    void ResetAnim()
+    {
+        if (IsRandom)
+        {
+            for (int i = 0; i < TargetData.Length; i++)
+            {
+                TargetData[i].TargetTime = Random.Range(0.1f, 0.5f);
+            }
+        }
         Target = 0;
-        timestamp = 0;
+        timestamp = Time.time + TargetData[Target].TargetTime + StartOffsetTime;
         TheColor = TargetData[Target].TargetColor;
         Speed = TargetData[Target].Speed;
         if (TheImg)
         {
-            TheImg.color =  TheColor;
+            TheImg.color = TheColor;
         }
         if (TheText)
         {
             TheText.color = TheColor;
+        }
+        if (TheOutline)
+        {
+            TheOutline.effectColor = TheColor;
         }
     }
     void Update()
     {
         if (timestamp < Time.time)
         {
-            timestamp = Time.time+TargetData[Target].TargetTime;
-            TheColor = TargetData[Target].TargetColor;
-
-            Speed = TargetData[Target].Speed;
-            Target += 1;
-            if (Target > TargetData.Length - 1)
+            if (Target == TargetData.Length)
             {
-                Target = 0;
+                if (_IsLoop)
+                {
+                    ResetAnim();
+                }
+
             }
+            else
+            {
+                timestamp = Time.time + TargetData[Target].TargetTime;
+                TheColor = TargetData[Target].TargetColor;
+
+                Speed = TargetData[Target].Speed;
+                Target += 1;
+            }
+
+
         }
         if (TheImg)
         {
@@ -53,6 +81,10 @@ public class ColorAnim : MonoBehaviour
         if (TheText)
         {
             TheText.color = Vector4.Lerp(TheText.color, TheColor, Speed * Time.deltaTime);
+        }
+        if (TheOutline)
+        {
+            TheOutline.effectColor = Vector4.Lerp(TheOutline.effectColor, TheColor, Speed * Time.deltaTime);
         }
     }
 }
